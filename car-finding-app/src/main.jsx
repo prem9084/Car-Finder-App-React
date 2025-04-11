dddd4cd7c2cc07ce5ef0f9c1a760c880c57a1651
prem9084 <146803260+prem9084@users.noreply.github.com>
2025-04-11 16:11:01 +0530

    add

--- car-finding-app/src/main.jsx
+++ car-finding-app/src/main.jsx
@@ -6,9 +6,11 @@ import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "./context/ThemeContext.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
-  </BrowserRouter>
+  <StrictMode>
+    <BrowserRouter>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
+  </StrictMode>
 );
